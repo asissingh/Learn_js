c3fdd91d3e0783b6eab6afd0c6a5cf228e476e33
ashish singh <ashishsinghrajput35@gmail.com>
2020-08-20 21:14:48 +0530

    UP_DOWN_Formated code

--- up_down_game.js
+++ up_down_game.js
@@ -1,9 +1,8 @@
-let a = [10,12,44,52,41,52,11];
-for(let i = 0;i<a.length-1;i++){
-if(a[i]<a[i+1]){
-    console.log(a[i+1]+" UP");
-}
-else{
-console.log(a[i+1]+" Down");
-}
+let a = [10, 12, 44, 52, 41, 52, 11];
+for (let i = 0; i < a.length - 1; i++) {
+  if (a[i] < a[i + 1]) {
+    console.log(a[i + 1] + " UP");
+  } else {
+    console.log(a[i + 1] + " Down");
+  }
 }
