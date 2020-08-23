920d4f0112580409cf84d5458c79fbf1347d72e2
ashish singh <ashishsinghrajput35@gmail.com>
2020-08-23 12:42:30 +0530

    somthig_change

--- src/even.js
+++ src/even.js
@@ -1,7 +1,7 @@
 for (let i = 1; i <= 10; i++) {
-  if (i % 2 == 0) {
-    console.log(i + ' is even no');
+  if (i % 2 === 0) {
+    console.log(`${i} is even no`);
   } else {
-    console.log(i + ' is odd no');
+    console.log(`${i} is odd no`);
   }
 }
